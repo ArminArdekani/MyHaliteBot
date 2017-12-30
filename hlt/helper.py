4127281863dbad6eb0666cfe41c0822a568f7e7a
ArminArdekani <armin.ardekani@gmail.com>
2017-12-29 23:03:39 -0500

    basic enemy planet/ship targetting and prep code for planet/ship statuses

---
+++ hlt/helper.py
@@ -0,0 +1,18 @@
+import hlt
+
+def are_all_planets_full(planets_status):
+	for planet_id, value in planets_status.items():
+		if value[1] == False:
+			return False
+	return True
+
+def closest_enemies(game_map, ship):
+	closest_enemy_ships = []
+	closest_enemy_planets = []
+	entities_by_distance = game_map.nearby_entities_by_distance(ship)
+	for distance in entities_by_distance:
+		if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in game_map.get_me().all_ships():
+			closest_enemy_ships.append(entities_by_distance[distance][0])
+		elif isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned():
+			closest_enemy_planets.append(entities_by_distance[distance][0])
+	return [closest_enemy_ships, closest_enemy_planets]
