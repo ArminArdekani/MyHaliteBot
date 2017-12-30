2eebe253a88360c321ab068449bbd9c0dc9e11d3
ArminArdekani <armin.ardekani@gmail.com>
2017-12-30 02:43:41 -0500

    added strategy of obtaining new planets first then enemy planets and ships (prioritized by distance)

--- MyBot.py
+++ MyBot.py
@@ -39,14 +39,15 @@ while True:
 		closest_enemies = helper.closest_enemies(game_map, ship);
 		closest_enemy_ships = closest_enemies[0]
 		closest_enemy_planets = closest_enemies[1]
-		
+		planets = closest_enemies[2]
+		planets.extend(closest_enemy_planets)
 		# If the ship is docked
 		if ship.docking_status != ship.DockingStatus.UNDOCKED:
 			# Skip this ship
 			continue
 			
 		# For each planet in the game (only non-destroyed planets are included)
-		for planet in game_map.all_planets():
+		for planet in planets:
 			# If the planet is owned
 			if planet.is_owned() or planet.is_full():
 				# Skip this planet
@@ -61,29 +62,28 @@ while True:
 				command_queue.append(ship.dock(planet))
 				queued=True
 			else:
-				if len(closest_enemy_planets) > 0:
-					# If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
-					# with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
-					# We run this navigate command each turn until we arrive to get the latest move.
-					# Here we move at half our maximum speed to better control the ships
-					# In order to execute faster we also choose to ignore ship collision calculations during navigation.
-					# This will mean that you have a higher probability of crashing into ships, but it also means you will
-					# make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
-					# wish to turn that option off.
-					navigate_command = ship.navigate(
-						ship.closest_point_to(closest_enemy_planets[0]),
-						game_map,
-						speed=int(hlt.constants.MAX_SPEED),
-						ignore_ships=False)
+				# If we can't dock, we move towards the closest empty point near this planet (by using closest_point_to)
+				# with constant speed. Don't worry about pathfinding for now, as the command will do it for you.
+				# We run this navigate command each turn until we arrive to get the latest move.
+				# Here we move at half our maximum speed to better control the ships
+				# In order to execute faster we also choose to ignore ship collision calculations during navigation.
+				# This will mean that you have a higher probability of crashing into ships, but it also means you will
+				# make move decisions much quicker. As your skill progresses and your moves turn more optimal you may
+				# wish to turn that option off.
+				navigate_command = ship.navigate(
+					ship.closest_point_to(planet),
+					game_map,
+					speed=int(hlt.constants.MAX_SPEED),
+					ignore_ships=False)
 				# If the move is possible, add it to the command_queue (if there are too many obstacles on the way
 				# or we are trapped (or we reached our destination!), navigate_command will return null;
 				# don't fret though, we can run the command again the next turn)
-					if navigate_command:
-						command_queue.append(navigate_command)
-						queued=True
+				if navigate_command:
+					command_queue.append(navigate_command)
+					queued=True
 			break
 		
-		if len(closest_enemy_planets) == 0 and queued==False:
+		if len(planets) == 0 and queued==False:
 			navigate_command = ship.navigate(
 				ship.closest_point_to(closest_enemy_ships[0]),
 				game_map,
--- hlt/helper.py
+++ hlt/helper.py
@@ -1,4 +1,5 @@
 import hlt
+import collections
 
 def are_all_planets_full(planets_status):
 	for planet_id, value in planets_status.items():
@@ -9,10 +10,13 @@ def are_all_planets_full(planets_status):
 def closest_enemies(game_map, ship):
 	closest_enemy_ships = []
 	closest_enemy_planets = []
+	free_planets = []
 	entities_by_distance = game_map.nearby_entities_by_distance(ship)
-	for distance in entities_by_distance:
+	for distance in sorted(entities_by_distance):
 		if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in game_map.get_me().all_ships():
 			closest_enemy_ships.append(entities_by_distance[distance][0])
+		elif isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and entities_by_distance[distance][0].get_planet_owner() == None:
+			free_planets.append(entities_by_distance[distance][0])
 		elif isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned():
 			closest_enemy_planets.append(entities_by_distance[distance][0])
-	return [closest_enemy_ships, closest_enemy_planets]
+	return [closest_enemy_ships, closest_enemy_planets, free_planets]
